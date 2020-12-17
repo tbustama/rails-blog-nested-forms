262b3b7146f443acc3bb00598af899e509e806b4
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-17 11:35:44 -0800

    commit tree 8600d97a6fa2a2b17b60b3ca4ba2fd3faae98b8d

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
-  accepts_nested_attributes_for :tags, reject_if: proc {|attributes| attributes["name"].blank? }
+  accepts_nested_attributes_for :tags, reject_if: proc {|attributes| attributes.blank? }
   validates_presence_of :name, :content
 
  
